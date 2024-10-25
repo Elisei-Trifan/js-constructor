908faa0d61e84fd9a05c99b873679c07f38c6a0e
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-25 22:54:44 +0300

    create new class

--- src/index.js
+++ src/index.js
@@ -2,14 +2,3 @@ import { model } from './model'
 import { Site } from './classes/site'
 import './styless/main.css'
 import { Sidebar } from './classes/sidebar'
-
-const site = new Site('#site')
-
-site.render(model)
-
-const updateCall = (newBlock) => {
-  model.push(newBlock)
-  site.render(model)
-}
-
-new Sidebar('#panel', updateCall)
